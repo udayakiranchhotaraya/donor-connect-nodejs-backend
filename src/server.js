d47458182f2cd2600fa5c88abba488ede852a402
udayakiranchhotaraya <112773247+udayakiranchhotaraya@users.noreply.github.com>
2025-04-08 18:05:33 +0530

    initial db setup

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
 require('dotenv').config();
-// const dbConnect = require('./db/db.config');
+const { PORT } = require('./config/config');
+const connectDB = require('./db/db.config');
 
 const app = require('./app');
 
-const PORT = process.env.PORT || 4001;
+// const PORT = process.env.PORT || 4001;
 const SERVER_URL = `http://localhost:${PORT}`;
 
 (async function () {
     try {
-        // await dbConnect();
+        await connectDB();
         app.listen(PORT, () => console.log(`Server started at ${SERVER_URL}`));
     } catch (error) {
         console.error(`Connection error: ${error}`);
